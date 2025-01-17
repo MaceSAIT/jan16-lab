f1f66d040060a578451e678b104ac1ce8be7d5ff
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2025-01-16 17:30:24 -0700

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const name: string = "Jane Doe";
+  let job: string = "Software Developer";
+  const age: number = 30;
+  const isEmployed: boolean = true;
+  const list: string[] = ["Thing 1","Thing 2","Thing 3"];
+
   return (
+
+    
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is the name {name}</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity onPress={() => alert("This is an alert.")}>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
